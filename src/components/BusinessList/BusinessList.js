308ff457c3c05975e9795200be6b37716bc307fb
tmehta2442 <tmehta@post.harvard.edu>
2017-12-29 21:15:42 -0500

    almost there...

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,8 +7,8 @@ class BusinessList extends React.Component {
     return (
     <div className="BusinessList">
       {
-        this.props.businesses.map(function(business){
-          return <Business business={business}/>;
+        this.props.businesses.map(business => {
+          return <Business business={business} key={business.id}/>;
         })
       }
     </div>
